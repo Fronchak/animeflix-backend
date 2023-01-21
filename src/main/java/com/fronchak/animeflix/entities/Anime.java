bfe1466d00231a8b859e43e4968f12f7d25a4ce8
Gabriel Fronchak Gmack <gabrielgmack@gmail.com>
2023-01-21 17:40:31 -0300

    Create entities

--- src/main/java/com/fronchak/animeflix/entities/Anime.java
+++ src/main/java/com/fronchak/animeflix/entities/Anime.java
@@ -1,12 +1,17 @@
 package com.fronchak.animeflix.entities;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -22,36 +27,70 @@ public class Anime {
 	@Column(name = "lauch_year")
 	private Integer lauchYear;
 	private Double avaliation;
+	@Column(name = "img_url")
+	private String imgUrl;
+	
+	@ManyToMany
+	@JoinTable(name = "anime_category",
+			joinColumns = @JoinColumn(name = "id_anime"),
+			inverseJoinColumns = @JoinColumn(name = "id_category"))
+	private Set<Category> categories = new HashSet<>();
+	
 	public Long getId() {
 		return id;
 	}
 	public void setId(Long id) {
 		this.id = id;
 	}
+	
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public String getSynopsis() {
 		return synopsis;
 	}
+	
 	public void setSynopsis(String synopsis) {
 		this.synopsis = synopsis;
 	}
+	
 	public Integer getLauchYear() {
 		return lauchYear;
 	}
+	
 	public void setLauchYear(Integer lauchYear) {
 		this.lauchYear = lauchYear;
 	}
+	
 	public Double getAvaliation() {
 		return avaliation;
 	}
+	
 	public void setAvaliation(Double avaliation) {
 		this.avaliation = avaliation;
 	}
+	
+	public String getImgUrl() {
+		return imgUrl;
+	}
+	
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+	
+	public Set<Category> getCategories() {
+		return categories;
+	}
+	
+	public void addCategory(Category category) {
+		categories.add(category);
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
